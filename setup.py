e2e72423ca8e6881f15a833ed1b279768e85a69c
rajupal <mail.rajupal@gmail.com>
2022-07-31 00:51:14 -0400

    My Python Project

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='dht11sensor',
+    version='',
+    packages=['dht11', 'dht11.rest', 'dht11.model', 'dht11.daemon', 'dht11.helper', 'dht11.logging', 'dht11.message',
+              'dht11.service', 'dht11.connector'],
+    url='',
+    license='',
+    author='rajupal',
+    author_email='',
+    description=''
+)
